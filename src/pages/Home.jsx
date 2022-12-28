fc4d89ac6124ede4b2c37c005656078d2177cb7a
andrii-pozniak <andriipozniak@gmail.com>
2022-12-28 23:28:32 +0200

    add image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,15 @@
-import { Flex, Container, Box, Image } from '@chakra-ui/react';
-
-const styles = {
-    container: {
-      minHeight: 'calc(100vh - 50px)',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontWeight: 500,
-      fontSize: 48,
-      textAlign: 'center',
-    },
-  };
+import { Heading, Box, Image, Center } from '@chakra-ui/react';
 
   export default function Home() {
     return (
         <Box >
-            <Container w='98%'>
-                <Flex w='98%' >
-                <div style={styles.container}>
-        <h1 style={styles.title}>
-          Task manager welcome page{' '}
-          <span role="img" aria-label="Greeting icon">
-            💁‍♀️
-            
-          </span>
-          
-        </h1>
-        <img src='/logo512.png'   alt='Dan Abramov'  />
-      </div>
-               
-                </Flex>
-               
-            </Container>
+             <Center>
+             <Heading as='h1' mt={'10'} size='2xl'>Phonebook manager welcome page</Heading>  
+              </Center>           
+            <Center>
+            <Image mt={'5'}  boxSize='400px' src='https://st.depositphotos.com/1001187/2640/v/600/depositphotos_26401401-stock-illustration-vector-phone-icon.jpg'   alt='Dan Abramov'  />
+
+            </Center>            
         </Box>
       
     );
