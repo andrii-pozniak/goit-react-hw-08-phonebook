d652ae2c6b35000c8d1ecd0b86d72c83f0f366a8
andrii-pozniak <andriipozniak@gmail.com>
2022-12-28 23:33:00 +0200

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { Heading, Box, Image, Center } from '@chakra-ui/react';
              <Heading as='h1' mt={'10'} size='2xl'>Phonebook manager welcome page</Heading>  
               </Center>           
             <Center>
-            <Image mt={'5'}  boxSize='400px' src='https://st.depositphotos.com/1001187/2640/v/600/depositphotos_26401401-stock-illustration-vector-phone-icon.jpg'   alt='Dan Abramov'  />
+            <Image mt={'5'}  src='https://st.depositphotos.com/1001187/2640/v/600/depositphotos_26401401-stock-illustration-vector-phone-icon.jpg'   alt='Dan Abramov'  />
 
             </Center>            
         </Box>
